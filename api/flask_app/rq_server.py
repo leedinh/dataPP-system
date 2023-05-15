0fbe470e4b01c2bb816ca52be4810f94037f78cd
dinhSKM <dinh.le@skymavis.com>
2023-05-16 02:55:48 +0700

    dockerize be service

--- api/flask_app/rq_server.py
+++ api/flask_app/rq_server.py
@@ -1,5 +1,4 @@
 from redis import Redis
 from rq import Queue
-import os
-redis_conn = Redis(host='localhost', port=6379)
-rq_queue = Queue('supa', connection=redis_conn, default_timeout=10)
+redis_conn = Redis(host='redis')
+rq_queue = Queue('supa', connection=redis_conn, default_timeout=3600)
